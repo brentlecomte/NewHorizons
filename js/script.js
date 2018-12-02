3530174e33ac79da0c984176bfa76c6a755df2b8
Brent Le Comte <brentlecomte@hotmail.com>
2018-12-02 21:40:55 +0100

    minder laggy nose movement

--- js/script.js
+++ js/script.js
@@ -41,28 +41,22 @@ import Satelite from "./classes/Satelite.js";
   const getPosenet = async () => {
     const net = await posenet.load();
     const pose = await net.estimateSinglePose(video, 0.5, true, 16);
-
-    runPosenet(pose);
-  };
-
-  const runPosenet = pose => {
     requestAnimationFrame(getPosenet);
     posePoints = pose;
     satelite.mesh.position.x = mapValue(
-      posePoints.keypoints[0].position.x,
+      pose.keypoints[0].position.x,
       0,
       600,
       0,
-      1
+      3
     );
     satelite.mesh.position.y = mapValue(
-      posePoints.keypoints[0].position.y,
+      pose.keypoints[0].position.y,
       0,
       600,
-      0,
-      1
+      2,
+      -2
     );
-    console.log(posePoints);
   };
 
   const mapValue = (value, istart, istop, ostart, ostop) =>
